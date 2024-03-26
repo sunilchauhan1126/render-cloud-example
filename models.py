4f542825daf7282578ab14fe909922bf59899f81
sunilchauhan1126 <156247062+sunilchauhan1126@users.noreply.github.com>
2024-03-26 14:16:38 +0530

    Update models.py

--- models.py
+++ models.py
@@ -17,8 +17,9 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
-    db.init_app(app)
-    db.create_all()
+    with app.app_context():
+      db.init_app(app)
+      db.create_all()
 
 
 '''
